7cccc901466d7cc6a1af45477dba5ca0c7ef8f87
Herve <habonimanah@gmail.com>
2024-09-23 15:39:21 -0500

    change version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="demo-hformat",
-    version="0.0.1",
+    version="0.0.3",
     description="Reformats files to stdout",
     install_requires = ["click", "pandas"],
     entry_points="""
