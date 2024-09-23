04dc5f70f1f8bb583971c979a5609e06c483f311
Herve <habonimanah@gmail.com>
2024-09-23 05:53:01 -0500

    removes python 3.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="demo-hformat",
+    name="hformat",
     version="0.0.1",
     description="Reformats files to stdout",
     install_requires = ["click", "pandas"],
